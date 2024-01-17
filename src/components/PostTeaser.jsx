23fa59b243a96a22fa8bb792bafbfbc930c75965
joshlarue <44442031+joshlarue@users.noreply.github.com>
2024-01-17 11:45:17 -0700

    post fetching no longer throws error. mapping posts into teasers still not functional

--- src/components/PostTeaser.jsx
+++ src/components/PostTeaser.jsx
@@ -1,67 +1,61 @@
 import { micromark } from "micromark";
 import DOMPurify from "dompurify";
 
-let posts;
+let posts = [];
 let postList = {
   "posts": []
 };
 
-async function PostTeaser() {
-  postList = await getPostList();
-  console.log(postList);
+function PostTeaser() {
+  getPostList();
+  console.log(postList.posts);
   return (
-    <>
-      <div className="teaser">
-        {postList.posts.slice().reverse().map((post, index) => ( // issue: if one file doesn't load then entire order is messed up
-          <>
-            <div className="teaser-bkg" key={post.id}>
-              <div className="teaser-l">
-                <div className="teaser-header">{post.title}</div>
-                <div className="teaser-text">{{__html: DOMPurify.sanitize(micromark(posts[index]))}}</div>
-                <div className="teaser-read-more">read more</div>
-                <div className="teaser-featured-tag">{ index === 0 ? "Featured article" : "" }</div>
-                <div className="teaser-date">{post.date}</div>
-              </div>
-              <div className="post-teaser-r">
-                <img className="teaser-img" src={post.img} alt={post.img-alt}></img>
-              </div>
-            </div>
-          </>
-        ))}
-
-      </div>
-    </>
+    <div className="teaser">
+      {postList.posts.map((post, index) => ( // issue: if one file doesn't load then entire order is messed up ---- postList.posts.slice().reverse().map()
+        <div className="teaser-bkg" key={post.id}>
+          <div className="teaser-l">
+            <div className="teaser-header">{post.title}</div>
+            <div className="teaser-text">{{__html: DOMPurify.sanitize(micromark(posts[index]))}}</div>
+            <div className="teaser-read-more">read more</div>
+            <div className="teaser-featured-tag">{ index === 0 ? "Featured article" : "" }</div>
+            <div className="teaser-date">{post.date}</div>
+          </div>
+          <div className="teaser-r">
+            <img className="teaser-img" src={post.img} alt={post.alt}></img>
+          </div>
+        </div>
+      ))}
+    </div>
   )
 }
 
+// need this for info to style teaser boxes :)
 async function getPostList() {
   const postListPath = '/blog/posts/postlist.json';
+  let postListJson;
   try {
     const response = await fetch(postListPath);
-    const postListJson = await response.json();
-    console.log(postListJson.posts);
-
-    postListJson.posts.forEach((post) => {
-      let formattedPost = {
-        "id": post.id,
-        "title": post.title,
-        "fileName": post.fileName,
-        "date": post.date,
-        "img": post.img,
-        "img-alt": post.img-alt
-      };
-      postList.posts.push(formattedPost);
-    })
-    
+    postListJson = await response.json();
   } catch (e) {
     console.error("Uh oh! " + e);
   }
-  posts = getPosts(postList);
+
+  postListJson.posts.forEach((post) => {
+    let formattedPost = {
+      "id": post.id,
+      "title": post.title,
+      "fileName": post.fileName,
+      "date": post.date,
+      "img": post.img,
+      "alt": post.alt
+    };
+    postList.posts.push(formattedPost);
+  })
+  getPosts(postList);
   return postList;
 }
 
 async function getPosts(postList) {
-  let posts = [];
   const postsDir = "/blog/posts";
   await Promise.all(postList.posts.map(async (post) => {
     const response = await fetch(`${postsDir}/${post.fileName}`);
@@ -70,7 +64,6 @@ async function getPosts(postList) {
       posts.push(postContent);
     }
   }))
-  return posts;
 }
 
 export default PostTeaser;
